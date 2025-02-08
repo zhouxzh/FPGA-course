0224bad0b41434b2d21cf39993eb8ecd520a0bdd
Xianzhong Zhou <zhouxzh@gdut.edu.cn>
2025-02-08 10:26:53 +0800

    修改deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd dist
 git init 
 git add -A 
 git commit -m 'deploy' 
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages 
+git push -f git@github.com:zhouxzh/FPGA-course.git master:gh-pages 
 cd -
