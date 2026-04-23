4fdd6846b8c8cc378eadd9e671402afd377c6f19
Xianzhong Zhou <zhouxzh@gdut.edu.cn>
2026-04-23 21:04:51 +0800

    修复第二章ppt的排版错误

--- presentation/chapter1/build.ps1
+++
@@ -1,1 +0,0 @@
-pandoc chapter1.md -o chapter1.tex --pdf-engine=xelatex -V CJKmainfont="Microsoft YaHei" --from markdown --to beamer --template "../../dist/eisvogel.beamer" --listings
--- presentation/chapter2/build.ps1
+++ presentation/chapter2/build.ps1
@@ -1,1 +1,25 @@
-pandoc chapter1.md -o chapter1.tex --pdf-engine=xelatex -V CJKmainfont="Microsoft YaHei" --from markdown --to beamer --template "../../dist/eisvogel.beamer" --listings
+$ErrorActionPreference = "Stop"
+
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $scriptDir
+
+$latexmk = Get-Command latexmk -ErrorAction SilentlyContinue
+if ($latexmk) {
+    latexmk -xelatex -interaction=nonstopmode -file-line-error -synctex=1 -halt-on-error chapter2.tex
+    if ($LASTEXITCODE -ne 0) {
+        throw "latexmk build failed"
+    }
+    exit 0
+}
+
+$xelatex = Get-Command xelatex -ErrorAction SilentlyContinue
+if (-not $xelatex) {
+    throw "Neither latexmk nor xelatex was found in PATH."
+}
+
+for ($pass = 1; $pass -le 2; $pass++) {
+    xelatex -interaction=nonstopmode -file-line-error -synctex=1 -halt-on-error chapter2.tex
+    if ($LASTEXITCODE -ne 0) {
+        throw "xelatex build failed on pass $pass"
+    }
+}
